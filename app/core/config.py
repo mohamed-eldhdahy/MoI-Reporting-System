34fbcf2858b01a7d143de01774325ba7ffc579d5
AhmedYMohamed <ahmedya618@gmail.com>
2025-12-07 17:11:25 +0200

    Allowing all Origins

--- app/core/config.py
+++ app/core/config.py
@@ -49,7 +49,7 @@ class Settings(BaseSettings):
     BLOB_CONTAINER_NAME: str = "report-attachments"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    ALLOWED_ORIGINS: list = ["http://localhost:3000", "http://localhost:8080", "http://localhost:56336","https://depi-moi-project.netlify.app/", "capacitor://localhost"]
+    ALLOWED_ORIGINS: list = ["*"]
     RATE_LIMIT_PER_MINUTE: int = 60
 
     class Config:
